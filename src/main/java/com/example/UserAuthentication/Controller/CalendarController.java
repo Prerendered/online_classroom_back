59dc667602b678ba2a4c9ccc3f0c308f82cf85ac
Dylan Seevathian <dylansvth@gmail.com>
2023-10-02 16:36:16 +0400

    Changes:Final calendar functions

--- src/main/java/com/example/UserAuthentication/Controller/CalendarController.java
+++ src/main/java/com/example/UserAuthentication/Controller/CalendarController.java
@@ -1,7 +1,7 @@
 package com.example.UserAuthentication.Controller;
 
 import com.example.UserAuthentication.Entity.Calendar;
-import com.example.UserAuthentication.Service.CalendarServiceService;
+import com.example.UserAuthentication.Service.CalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/UserAuthentication/Repo/CalendarRepo.java
+++ src/main/java/com/example/UserAuthentication/Repo/CalendarRepo.java
@@ -1,6 +1,6 @@
 package com.example.UserAuthentication.Repo;
 
-import com.example.UserAuthentication.Entity.Forum;
+import com.example.UserAuthentication.Entity.Calendar;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CalendarRepo extends MongoRepository<Calendar, String>
