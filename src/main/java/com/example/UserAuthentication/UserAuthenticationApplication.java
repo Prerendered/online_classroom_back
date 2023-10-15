b40e1da92de040857d3a06605735cb74dc092b61
Prerendered <20709154@student.curtin.edu.au>
2023-10-15 23:55:13 +0400

    changes: added calendar

--- src/main/java/com/example/UserAuthentication/UserAuthenticationApplication.java
+++ src/main/java/com/example/UserAuthentication/UserAuthenticationApplication.java
@@ -11,11 +11,4 @@ public class UserAuthenticationApplication {
 	public static void main(String[] args) { 
 		SpringApplication.run(UserAuthenticationApplication.class, args); 
 	}
-
-	// Post a question and an answer in the forum
-
-
-	// Get all the questions and answers in the forum
-
-
 }
